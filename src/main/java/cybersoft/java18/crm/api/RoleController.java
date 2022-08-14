9efb47e1b57ff98c21099fff05d6c46826bf1d62
checongbinh20001990 <checongbinh20011990@gmail.com>
2022-08-14 17:11:05 +0700

    Buoi 16 Return JSON

---
+++ src/main/java/cybersoft/java18/crm/api/RoleController.java
@@ -0,0 +1,50 @@
+package cybersoft.java18.crm.api;
+
+import com.google.gson.Gson;
+import cybersoft.java18.crm.model.RoleModel;
+import cybersoft.java18.crm.utils.UrlUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// api/role
+@WebServlet(name = "role",urlPatterns = {
+        UrlUtils.URL_ROLE,
+        UrlUtils.URL_ROLE_ADD
+})
+public class RoleController extends HttpServlet {
+
+    private Gson gson = new Gson();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("kiemtra do get");
+
+        RoleModel roleModel = new RoleModel();
+        roleModel.setId(1);
+        roleModel.setName("Nguyễn Văn Test");
+        roleModel.setDescription("Đây là mô tả về JSON");
+
+        String json = gson.toJson(roleModel);
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print(json);
+        printWriter.flush();
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("kiemtra do post");
+    }
+
+
+}
