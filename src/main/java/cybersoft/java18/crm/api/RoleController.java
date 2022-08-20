c51d88b4e97194c2bac7d3870785010fd00c10d6
checongbinh20001990 <checongbinh20011990@gmail.com>
2022-08-20 18:01:05 +0700

    connect jdbc crm

--- src/main/java/cybersoft/java18/crm/api/RoleController.java
+++ src/main/java/cybersoft/java18/crm/api/RoleController.java
@@ -2,6 +2,7 @@ package cybersoft.java18.crm.api;
 
 import com.google.gson.Gson;
 import cybersoft.java18.crm.model.RoleModel;
+import cybersoft.java18.crm.services.RoleServices;
 import cybersoft.java18.crm.utils.UrlUtils;
 
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 // api/role
 @WebServlet(name = "role",urlPatterns = {
@@ -25,15 +27,18 @@ public class RoleController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("kiemtra do get");
 
-        RoleModel roleModel = new RoleModel();
-        roleModel.setId(1);
-        roleModel.setName("Nguyễn Văn Test");
-        roleModel.setDescription("Đây là mô tả về JSON");
+//        RoleModel roleModel = new RoleModel();
+//        roleModel.setId(1);
+//        roleModel.setName("Nguyễn Văn Test");
+//        roleModel.setDescription("Đây là mô tả về JSON");
 
-        String json = gson.toJson(roleModel);
+        List<RoleModel> listRoles =  RoleServices.getInstance().getAllRole();
+
+        String json = gson.toJson(listRoles);
 
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
         req.setCharacterEncoding("UTF-8");
         PrintWriter printWriter = resp.getWriter();
         printWriter.print(json);
