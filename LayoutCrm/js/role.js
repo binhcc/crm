942c5c72d5c8dbf7d63d4fc41afb8af2fff306d5
checongbinh20001990 <checongbinh20011990@gmail.com>
2022-08-21 17:07:55 +0700

    add api update, api delete

--- LayoutCrm/js/role.js
+++ LayoutCrm/js/role.js
@@ -15,12 +15,39 @@ $(document).ready(function(){
                 <td>${value.description}</td>
                 <td>
                     <a href="#" class="btn btn-sm btn-primary">Sửa</a>
-                    <a href="#" class="btn btn-sm btn-danger">Xóa</a>
+                    <a href="#" class="btn btn-sm btn-danger btn-delete" role-id="${value.id}">Xóa</a>
                 </td>
             </tr>`
 
             $("#example tbody").append(row)
         })
+    })
+
+    $('body').on('click','.btn-delete',function(){
+        // Lấy thuộc tính role-id từ button được click
+        var roleId = $(this).attr('role-id')
+        console.log(`Role id ${roleId}`)
+        // $(this).closest('tr').remove()
+        // location.reload()
+
+        $.ajax({
+            url: `http://localhost:8080/crm_war/api/role?id=${roleId}`, //Link lấy danh sách role
+            method: "DELETE", //Phương thức tương ứng với link
+            // data: {
+            //     key: value
+            // },
+            // dataType: "application/json"
+        }).done(function(result){
+            console.log("kiemtra")
+            console.log(result)
+            // Lấy thành công và trả ra kết quả
+            if(result.isSuccess == true){
+                console.log("Xoá thành công !")
+            }else{
+                console.log("Xoá thất bại !")
+            }
+        })
+        
     })
 
 })
